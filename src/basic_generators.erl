f5f944ca838123eac621e7ca99a5a52f8e887f7e
Samuel Rivas <samuel.rivas@klarna.com>
2013-11-09 12:33:48 +0100

    Solutions for the basic_generators exercise

--- src/basic_generators.erl
+++ src/basic_generators.erl
@@ -8,44 +8,51 @@
 
 %% A dice throw (i.e. an integer in [1, 6])
 die_throw() ->
-  false.
+  proper_types:choose(1, 6).
 
 %% Two dice throws, e.g. {1, 6}, {2, 4}, ...
 two_throws() ->
-  false.
+  {die_throw(), die_throw()}.
 
 %% Generate things like a, b, ..., z
 small_letter() ->
-  false.
+  proper_types:choose($a, $z).
 
 %% Generate things like A, B, ..., Z
 big_letter() ->
-  false.
+  proper_types:choose($A, $Z).
 
 %% Either a big or a small letter
 letter() ->
-  false.
+  proper_types:oneof([big_letter(), small_letter()]).
 
 %% Generate strings like "askfjKKEp"
 printable_string() ->
-  false.
+  proper_types:list(letter()).
 
 %% A sentence. This should generate a lower case string with words (random lists
 %% of letters) of around 5 letters.
 %% Don't worry about duplicated spaces (for now :))
 sentence() ->
-  false.
+  Spacey =
+    proper_types:frequency(
+      [{1, space()},
+       {5, small_letter()}]),
+  proper_types:list(Spacey).
 
 space() -> $ .
 
 %% A printable string starting with capital letter, like "Askfjkkep"
 'PrintableString'() ->
-  false.
+  [big_letter() | proper_types:list(small_letter())].
 
 %% A gb_set (use integers as elements)
 %% Hint, this may be hard
 gb_set() ->
-  false.
+  %% This doesn't work! gb_sets:from_list needs an actual list, not a proper
+  %% generator!
+  %% We'll see how to implement this later
+  gb_sets:from_list(proper_types:list(proper_types:integer())).
 
 %%%_* Emacs ============================================================
 %%% Local Variables:
