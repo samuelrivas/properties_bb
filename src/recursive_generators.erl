9f9ea58b305247270f2f17ef75d11814a0c59e3e
Samuel Rivas <samuel.rivas@klarna.com>
2013-11-11 14:14:01 +0100

    Basic version of recursive generators

---
+++ src/recursive_generators.erl
@@ -0,0 +1,51 @@
+%%% @doc
+-module(recursive_generators).
+
+-compile([export_all]).
+
+%%%_* Includes =========================================================
+-define(PROPER_NO_IMPORTS, true). %% I hate imported functions!
+-include_lib("proper/include/proper.hrl").
+
+%%%_* Warm up generator ================================================
+
+%% Generate a permutation of L. E.g.
+%% permutations([1, 2, 3]) ->
+%%  * [2, 1, 3]
+%%  * [1, 2, 3]
+%%  * [3, 2, 1]
+%%  * [2, 3, 1]
+%%  * ...
+permutation(L) ->
+  false.
+
+%%%_* Production Code ==================================================
+
+%%% Assume we want to test this function that folds arithmetic expressions of
+%%% the specified types
+
+-type expr() :: integer() | {expr(), 'x', expr()} | {expr(), '+', expr()}.
+
+eval({A, '+', B}) -> eval(A) + eval(B);
+eval({A, '*', B}) -> eval(A) * eval(B);
+eval(A) when is_integer(A) -> A.
+
+expr_type({_, '*', _})          -> times;
+expr_type({_, '+', _})          -> sum;
+expr_type(N) when is_integer(N) -> constant.
+
+%%%_* Properties =======================================================
+
+%%% Weak property, we just want to make sure we hit all the expression branches
+prop_eval() ->
+  ?FORALL(E, expr(), is_integer(eval(E))).
+
+%%%_* Generator =======================================================
+expr() -> proper_types:integer().
+
+
+%%%_* Emacs ============================================================
+%%% Local Variables:
+%%% allout-layout: t
+%%% erlang-indent-level: 2
+%%% End:
