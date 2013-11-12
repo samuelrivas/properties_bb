a1801102d68ed1e6a98dfedd627d0af50b3ff692
Samuel Rivas <samuel.rivas@klarna.com>
2013-11-12 15:24:29 +0100

    Final implementation of the template library

--- src/template.erl
+++ src/template.erl
@@ -8,7 +8,16 @@
 -type tok() :: at | {string, string()}.
 
 -spec string(string(), {string(), string()}) -> string().
-string(String, _Subs) -> String.
+string(String, Subs) ->
+    apply_subs(parse(tokens(String)), Subs).
+
+apply_subs(Parsed, Subs) ->
+    lists:concat([to_string(X, Subs) || X <- Parsed]).
+
+to_string({text, Text}, _Subs) ->
+    Text;
+to_string({var, Var}, Subs) ->
+    keysearch(Var, Subs).
 
 -spec tokens(string()) -> [tok()].
 tokens([]) ->
@@ -34,3 +43,11 @@ switch_terminal(text) ->
     var;
 switch_terminal(var) ->
     text.
+
+keysearch(Key, List) ->
+    case lists:keyfind(Key, 1, List) of
+        {Key, Value} ->
+            Value;
+        false ->
+            throw({not_found, {Key, List}})
+    end.
